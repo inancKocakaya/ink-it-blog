8eceee3fa034c6aeae6642c7266c7cca7b1bc5a4
inancKocakaya <inanc.kocakaya@gmail.com>
2025-10-04 15:54:16 +0200

    Configure deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+    site: "https://inanckocakaya.github.io/",
+    base: "/ink-it-blog/",
     integrations: [mdx(), sitemap(), tailwind()],
 });
